17f10f37b8e7401eeadb9f95df7045a093aba988
linkouille <linkouillou@gmail.com>
2020-10-23 00:38:39 +0200

    Je perd du temps comme dab

---
+++ Assets/Script/SimpleKeyBoardMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class SimpleKeyBoardMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed;
+    }
+
+}
---
+++ Assets/Script/Sprite3DRender.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Sprite3DRender : MonoBehaviour
+{
+    public Texture text;
+
+    void Update()
+    {
+        if(text != GetComponent<Renderer>().material.mainTexture)
+            GetComponent<Renderer>().material.mainTexture = text;
+    }
+}
